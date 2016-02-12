88b09ef4b5f418d3982dca664d5e50028004f1b5
brian_schwartz <bjs9yv@virginia.edu>
2016-02-12 17:05:40 -0500

    finished Merchant class

--- marketplace/models.py
+++ marketplace/models.py
@@ -1,8 +1,20 @@
 from django.db import models
 
-class merchant(models.Model):
+class Merchant(models.Model):
     username = models.CharField(max_length=11)
     publickey = models.TextField()
     bitcoin_credit = models.FloatField()
+    accounts = models.TextField()
+    rating = models.IntegerField(default=0)
+    is_seller = models.BooleanField(default=False)
+    purchases = models.ForeignKey(Listing)
+    products = models.ForeignKey(Listing)
+    
+class Message(models.Model):
+    sender = models.ForeignKey(Merchant, related_name='message_sender')
+    recipient = models.ForeignKey(Merchant, related_name='message_recipient')
+    body = models.TextField()
+    opened = models.BooleanField(default=False)
+
     
     
