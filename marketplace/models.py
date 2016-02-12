310a0597066c0402612a5efc8ca14e0a9a464e2b
Reina Hernandez <reinaH@Reinas-MacBook-Pro.local>
2016-02-12 17:00:24 -0500

    Add Listing model

--- marketplace/models.py
+++ marketplace/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
-class merchant(models.Model):
+class Merchant(models.Model):
     username = models.CharField(max_length=11)
     publickey = models.TextField()
     bitcoin_credit = models.FloatField()
+    listing = models.ForeignKey(Listing)
     
     
+class Listing (models.Model):
+    title = models.TextField()
+    description = models.TextField()
+    bitcoin_cost = models.FloatField()
+
+    quantity_available = models.FloatField()
