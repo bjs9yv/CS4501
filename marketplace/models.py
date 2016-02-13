cd58470dd74edb6cd8c44a159b8bab4c488e85df
brian_schwartz <bjs9yv@virginia.edu>
2016-02-13 13:56:52 -0500

    Added Listing back

--- marketplace/models.py
+++ marketplace/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 import datetime
 
+class Listing (models.Model):
+    title = models.TextField()
+    description = models.TextField()
+    bitcoin_cost = models.FloatField()
+    quantity_available = models.FloatField()
+
 class Merchant(models.Model):
     username = models.CharField(max_length=11)
     publickey = models.TextField()
@@ -40,24 +46,20 @@ class Transaction(models.Model):
     TRANSACTION_CANCELLED = 3
     TRANSACTION_FAILED = 4
 
-    TRANSACTION_STATUSES = [(TRANSACTION_UNINITIATED, 'Transaction uninitiated'),
-                            (TRANSACTION_INCOMPLETE, 'Transaction incomplete'), 
-                            (TRANSACTION_COMPLETE, 'Transaction complete'),
-                            (TRANSACTION_CANCELLED, 'Transaction cancelled'),
-                            (TRANSACTION_CANCELLED, 'Transaction failed')]
+    TRANSACTION_STATUSES = [(TRANSACTION_UNINITIATED, 'Transaction uninitiated'),(TRANSACTION_INCOMPLETE, 'Transaction incomplete'),(TRANSACTION_COMPLETE, 'Transaction complete'),(TRANSACTION_CANCELLED, 'Transaction cancelled'),(TRANSACTION_CANCELLED, 'Transaction failed')]
     
     status = models.IntegerField(choices=TRANSACTION_STATUSES, default=0)
 
     def update_status(self, status):
         self.status = status
-	if(status == 1):
-	    self.shipped_date = models.DateTimeField(added_now=True)
+        if(status == 1):
+            self.shipped_date = models.DateTimeField(added_now=True)
 	if(status == 2):
-	    self.received_date = models.DateTimeField(added_now=True)
+            self.received_date = models.DateTimeField(added_now=True)
 	if(status == 3):
-	    self.cancelled_date = models.DateTimeField(added_now=True)
+            self.cancelled_date = models.DateTimeField(added_now=True)
 	if(status == 4):
-	    self.failed_date = models.DateTimeField(added_now=True)
+            self.failed_date = models.DateTimeField(added_now=True)
     
     def get_status(self):
 	return str(self.status)
