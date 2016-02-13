0e3494f3a1cf60e40846685356744a74c8b16c86
brian_schwartz <bjs9yv@virginia.edu>
2016-02-13 18:52:37 -0500

    views listingviewset added

---
+++ marketplace/views.py
@@ -0,0 +1,9 @@
+from rest_framework import viewsets
+from serializers import ListingSerializer
+
+class ListingViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows listings to be viewed or edited.
+    """
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
