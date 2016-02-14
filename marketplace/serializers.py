1fa6d41ce39d7a15a27ad56b0c3401ebd7c2c1a5
brian_schwartz <bjs9yv@virginia.edu>
2016-02-13 19:47:07 -0500

    I cant spell or import things

--- marketplace/serializers.py
+++ marketplace/serializers.py
@@ -1,22 +1,22 @@
 from rest_framework import serializers
-from models import Listing, Merchant, Message
+from .models import Listing, Merchant, Message
 
-class ListingSerializer(serializers.HyperlinedModelSerializer):
+class ListingSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Listing
         fields = ('title','description','bitcoin_cost','quantity_available')
         
-class TransactionSerializer(serializers.HyperlinedModelSerializer):
+class TransactionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Transaction
         fields = ('product','quantity', 'buyer', 'seller', 'today', 'shipped_date','received_date', 'cancelled date', 'failed_date', 'status')
         
-class MessageSerializer(serializers.HyperlinedModelSerializer):
+class MessageSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Message
         fields = ('sender','recipient','body','opened')
         
-class MerchantSerializer(serializers.HyperlinedModelSerializer):
+class MerchantSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Merchant
         fields = ('username','publickey','bitcoin_credit','accounts', 'rating', 'is_seller', 'purchases')
