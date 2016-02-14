9791537900108161b5ac87d97de3ebce83021b25
MerlinZhang <mz6ws@virginia.edu>
2016-02-13 19:23:03 -0500

    Updated serializers

--- marketplace/serializers.py
+++ marketplace/serializers.py
@@ -4,4 +4,20 @@ from models import Listing, Merchant, Message
 class ListingSerializer(serializers.HyperlinedModelSerializer):
     class Meta:
         model = Listing
-        fields = ('tite','description','bitcoin_cost','quantity_available')
+        fields = ('title','description','bitcoin_cost','quantity_available')
+        
+class TransactionSerializer(serializers.HyperlinedModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = ('product','quantity', 'buyer', 'seller', 'today', 'shipped_date','received_date', 'cancelled date', 'failed_date', 'status')
+        
+class MessageSerializer(serializers.HyperlinedModelSerializer):
+    class Meta:
+        model = Message
+        fields = ('sender','recipient','body','opened')
+        
+class MerchantSerializer(serializers.HyperlinedModelSerializer):
+    class Meta:
+        model = Merchant
+        fields = ('username','publickey','bitcoin_credit','accounts', 'rating', 'is_seller', 'purchases')
+        
