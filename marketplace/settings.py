b28578a58941c02372c780e10b3944febf081164
brian_schwartz <bjs9yv@virginia.edu>
2016-02-24 20:19:18 -0500

    api authentication allows all

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = (
 )
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.AllowAny',),
     'PAGE_SIZE': 10
 }
 
