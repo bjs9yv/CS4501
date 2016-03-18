26b00422af7c2c960ca0268e959ebf2d11b40438
MerlinZhang <mz6ws@virginia.edu>
2016-03-18 16:59:54 -0400

    Included relevant new urls

--- exp/experience_API/urls.py
+++ exp/experience_API/urls.py
@@ -5,5 +5,7 @@ from experience_API import views
 urlpatterns = [
     url(r'^listing_service/', views.listing_service, name='listing_service'),
     url(r'^recent_listings/', views.recent_listings, name='recent_listings'),
+    url(r'^login/', views.login, name='login'),
+    url(r'^create_user/', views.create_user, name='create_user'),
     url(r'^admin/', include(admin.site.urls)),
 ]
