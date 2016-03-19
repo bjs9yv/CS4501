31243da8c45487825473e9c6a8cd6077b015c716
brian_schwartz <bjs9yv@virginia.edu>
2016-03-19 13:21:09 -0400

    web level account creation only missing API call

--- web/web_interface/views.py
+++ web/web_interface/views.py
@@ -1,18 +1,38 @@
 from django.http import HttpResponse
-from urllib.request import urlopen
+from django.template.response import TemplateResponse
 from django.shortcuts import render
+from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.debug import sensitive_post_parameters
+from urllib.request import urlopen
 import urllib.request
 import urllib.parse
 import json
+from .forms import RegistrationForm
 
 def login(request):
     pass
 
+@sensitive_post_parameters('username', 'password1', 'password2')
+@csrf_protect
+@never_cache
 def create_account(request):
-    pass
+    if request.method == "POST":
+        form = RegistrationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            return HttpResponse(username + ' ' + password)
+            # make API call to exp layer
+            # HTTPReddirect to home if successful
+    else:
+        form = RegistrationForm()
+    context = {'form': form}
+    return render(request, 'create_account.html', context)
 
 def home(request):
-    req = urllib.request.Request('http://exp-api:8000/recent_listings')
+    url = 'http://exp-api:8000/recent_listings'
+    req = urllib.request.Request(url)
     resp_json = urllib.request.urlopen(req).read().decode('utf-8')
     resp = json.loads(resp_json)    
     return render(request, 'homepage.html', resp)
