41c7f6e65bb2ae849266d2d1c0ffe648a712e954
brian_schwartz <bjs9yv@virginia.edu>
2016-03-19 13:21:49 -0400

    web level account creation only missing API call

---
+++ web/web_interface/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+class RegistrationForm(forms.Form):
+    username = forms.CharField(label='Username', max_length=15, label_suffix='')
+    password1 = forms.CharField(label='Password', label_suffix='', widget=forms.PasswordInput())
+    password2 = forms.CharField(label='Confirm Password',label_suffix='', widget=forms.PasswordInput())
+    
+    def clean(self):
+        if self.cleaned_data.get('password1') != self.cleaned_data.get('password2'):
+            self.add_error('password2','Passwords much match')
+        # TODO: password too short
+        # TODO: username already taken, do this with api call to exp layer -> model layer
+        return self.cleaned_data
