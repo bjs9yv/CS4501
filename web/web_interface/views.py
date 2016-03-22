f30411a08d2342c3ef7eca607c4fe54a4f13c235
Merlin Zhang <mz6ws@virginia.edu>
2016-03-21 21:43:00 -0400

    Fixed syntax errors

--- web/web_interface/views.py
+++ web/web_interface/views.py
@@ -100,7 +100,7 @@ def create_listing(request):
             bitcoin_cost = form.cleaned_data['bitcoin_cost']
             quantity_available = form.cleaned_data['quantity_available']
 
-            resp = create_listing_exp_api(
+            resp = create_listing_exp_api()
             if not resp or not resp['ok']:
                 errors = resp['response']
                 context = {'form': form, 'errors': errors}
@@ -153,5 +153,6 @@ def logout_exp_api(auth):
     resp = json.loads(resp_json)
     return resp
 
-def create_listing_exp_api(...):
+def create_listing_exp_api():
+    pass
     
