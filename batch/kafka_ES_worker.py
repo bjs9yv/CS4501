3972afb952a391fde62d6b2a58e5c64346c4d425
brian_schwartz <bjs9yv@virginia.edu>
2016-03-31 15:06:07 -0400

    Kafka consumer works, ES pusher works

--- batch/kafka_ES_worker.py
+++ batch/kafka_ES_worker.py
@@ -1,11 +1,16 @@
 from kafka import KafkaConsumer
 import json
 import time
+from elasticsearch import Elasticsearch
  
-# Wait for 5 seconds
-time.sleep(5)
+# Wait for 15 seconds to make sure Kafka is up and running
+time.sleep(15)
 while True:
     consumer = KafkaConsumer('new-listings-topic', group_id='listing-indexer', bootstrap_servers=['kafka:9092'])
+    es = Elasticsearch(['es'])
     for message in consumer:
+        # take a message from the queue
         new_listing = json.loads((message.value).decode('utf-8'))
         # push to ES
+        es.index(index='listing_index', doc_type='listing', id=json.loads(new_listing)['id'], body=new_listing)
+        es.indices.refresh(index="listing_index")
