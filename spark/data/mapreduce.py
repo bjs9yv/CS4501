b5a9c662f835fdf814966b0ea958d5dc1d5b85e0
brian_schwartz <bjs9yv@virginia.edu>
2016-05-01 15:11:39 -0400

    spark up and running

--- spark/data/mapreduce.py
+++ spark/data/mapreduce.py
@@ -1,9 +1,10 @@
 from pyspark import SparkContext
 sc = SparkContext("spark://spark-master:7077", "PopularItems")
-data = sc.textFile("access.log", 2)
-pairs = data.map(lamda line: line.split("\t"))
-pages = pairs.map(lamda pair: (pair[1], 1))
-count = pages.reduceByKey(lamda x,y: x+y)
+data = sc.textFile("/tmp/data/access.log", 2)
+pairs = data.map(lambda line: line.split("\t"))
+
+pages = pairs.map(lambda pair: (pair[1], 1))
+count = pages.reduceByKey(lambda x,y: x+y)
 
 output = count.collect()
 for page_id, count in output:
