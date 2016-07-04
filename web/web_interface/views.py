3dbda7bc73b73dfaa714f6470eef1156cda7af1c
brian_schwartz <bjs9yv@virginia.edu>
2016-07-04 12:35:32 -0400

    add to cart button on hompage listings

--- web/web_interface/views.py
+++ web/web_interface/views.py
@@ -9,36 +9,26 @@ from django.views.decorators.debug import sensitive_post_parameters
 import requests
 import json
 from .forms import RegistrationForm, LoginForm, CreateListingForm
+from .exp import *
 from django.shortcuts import render_to_response
 
-# TODO: add to cart button on listings
-# TODO: cart.html
+
 def cart(request):
     auth = request.COOKIES.get('auth')
     if not auth:
-        # Currently you must be logged in to use the shopping cart, TODO is guest  with cookies
+        # Currently you must be logged in to use the shopping cart, TODO is guest with cookies
         return HttpResponseRedirect(reverse("login") + "?next=" + reverse("create_listing"))
     if request.method == "GET":
         context = {'auth': auth}
         if 'id' in request.GET:
             resp = add_to_cart_api(request.GET['id'], auth) # TODO: this mean auth needs to be remade each time
             context['response'] = resp['response']
-        
         # show the user their cart's items
         cart = get_cart_service(auth)
         context['items'] = cart['items']
         context['total'] = cart['total']
-
-
-        # TODO: add to cart button on listings
         return render(request, 'cart.html', context)
     return HttpResponseRedirect('home')
-
-"""
->>> d
-'[{"pk": 1, "fields": {"title": "MAGA Hat", "bitcoin_cost": 2016.0, "quantity_available": 9001.0, "description": "Signed by Trump"}, "model": "marketplace.listing"}]'
-"""
-
 
 def search(request):
     auth = request.COOKIES.get('auth')
@@ -158,58 +148,3 @@ def listing(request, listing_id):
     url += str(listing_id)
     resp = requests.get(url).json()
     return render(request, 'listing.html', resp)
-
-"""
-The remaining methods are API calls to the experience API layer
-We pull these out to make the code more readable above
-"""
-def recent_listings_exp_api():
-    url = 'http://exp-api:8000/recent_listings'
-    resp = requests.get(url).json()
-    return resp
-
-def create_account_exp_api(username, password):
-    url = 'http://exp-api:8000/create_user/'
-    url += '?username=%s&password=%s' % (username, password)
-    resp = requests.get(url).json()
-    return resp
-
-def login_exp_api(username, password):
-    url = 'http://exp-api:8000/login/'
-    url += '?username=%s&password=%s' % (username, password)
-    resp = requests.get(url).json()
-    return resp
-
-def logout_exp_api(auth):
-    url = 'http://exp-api:8000/logout/'
-    url += '?auth=%s' % (auth)
-    resp = requests.get(url).json()
-    return resp
-
-@csrf_exempt
-def create_listing_exp_api(title, description, bitcoin_cost, quantity_available):
-    url = 'http://exp-api:8000/create_listing_service/?'
-    postdata = {'title': title,
-                'description': description,
-                'bitcoin_cost': bitcoin_cost,
-                'quantity_available': quantity_available}
-    resp = requests.post(url, data=postdata)
-    return resp
-
-def search_exp_api(query):
-    url = 'http://exp-api:8000/search_results_service/'
-    url += '?query=%s' % (query)
-    resp = requests.get(url).json()
-    return resp
-
-def add_to_cart_api(listing_id, auth):
-    url = 'http://exp-api:8000/add_to_cart_service/'
-    url += '?id=%s&auth=%s' % (listing_id, auth)
-    resp = requests.get(url).json()
-    return resp
-
-def get_cart_service(auth):
-    url = 'http://exp-api:8000/get_cart_service/'
-    url += '?auth=%s' % (auth)
-    resp = requests.get(url).json()
-    return resp
